c0972f7ce46424fcb37e7a9165c0f716b967ffbe
覃止圣 <qinzhisheng@ly-sky.com>
2020-10-30 18:20:08 +0800

    first commit

---
+++ src/config/layoutMenu.js
@@ -0,0 +1,26 @@
+/**
+ * 底部菜单属性配置
+ */
+export const TabBarData=[
+    {
+        id: 'home',
+        name: '首页',
+        icon: require('images/recycleH5_07.png'),
+        selectedicon: require('images/recycleH5_02.png'),
+        url: '/home',
+      },
+      {
+        id: 'class',
+        name: '分类',
+        icon: require('images/recycleH5_03.png'),
+        selectedicon: require('images/recycleH5_06.png'),
+        url: '/class',
+      },
+      {
+        id: 'my',
+        name: '我的',
+        icon: require('images/recycleH5_04.png'),
+        selectedicon: require('images/recycleH5_05.png'),
+        url: '/my',
+      }
+];
---
+++ src/layouts/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import BaseLayout from './baseLayout';
+import { TabBarData } from "inconf/layoutMenu";
+
+
+//以下数组表示在当前页面展示的路由，使标签栏始终显示，
+//如果在数组中去掉'/my'，BaseLayout有一个标签跳转到'/my',点击标签跳转时，会重定向到'/my'
+//const ULR_NO_LAYOUT = ['/', '/home', '/class', '/my'];
+
+class Index extends Component {
+  initPath = () =>{
+    const ULR_NO_LAYOUT=['/'];
+    TabBarData.map((item)=>{
+      ULR_NO_LAYOUT.push(item.url);
+    });
+    return ULR_NO_LAYOUT;
+  }
+
+
+  componentDidMount() {
+  }
+  renderBody = () => {
+    const {location: {pathname}, children } = this.props;
+    if (this.initPath().includes(pathname)) {
+		console.log(pathname);
+      return  (<BaseLayout {...this.props} />);
+    }
+    return (
+	//<React.Fragment>等价于<></>
+      <React.Fragment>
+        {children}
+      </React.Fragment>
+    );
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.renderBody()}
+      </React.Fragment>
+    )
+  }
+}
+
+export default Index;
