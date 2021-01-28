26edcb55ba3391b6218ff26a028e155cd67d1838
WX <42308734@qq.com>
2021-01-28 15:52:46 +0800

    fix:详情页导航栏

--- src/components/HeaderCard/index.js
+++ src/components/HeaderCard/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { Card,Icon } from 'antd-mobile';
+import { Card } from 'antd-mobile';
 import styles from './index.less'
 import star from '../../assets/星星.png'
-import more from '../../assets/更多,三个点.png'
-import router from 'umi/router';
 const HeadCard = ({
   headImage,// 头像
   name, // 昵称
@@ -17,26 +15,22 @@ const HeadCard = ({
     <div>
       <Card full {...props}>
         <Card.Body style={{ paddingTop: 20, paddingRight: 20, paddingLeft: 20, paddingBottom: 20 }}>
-          <div style={{display:'flex',justifyContent:'space-between',marginBottom:5}}>
-            <div onClick={()=>router.goBack()}><Icon type="left" /></div>
-            <img className={styles.sex_icon}  src={more} alt="" />
-          </div>
-          <div style={{display:'flex',justifyContent:'space-between'}}>
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
             <div>
               <div style={{ display: 'flex', flexDirection: 'row' }}>
                 <img className={styles.head_logo} src={headImage} alt="" />
                 <div>
                   <div className={styles.real_name}>
-                    <span>Sweetheart❤恋人<img src={sexIcon} alt="" className={styles.sex_icon} /></span>
+                    <span>{nickName&&nickName||''}<img src={sexIcon} alt="" className={styles.sex_icon} /></span>
                   </div>
-                  <div className={styles.en_name}>昵称:{name && name || 'ღ'}</div>
-                  <div className={styles.en_name}>微信号:{wxName && wxName || 'micomxx'}</div>
-                  <div className={styles.en_name}>地区:{address && address || '江苏 南京'}</div>
+                  <div className={styles.en_name}>昵称:{name && name || ''}</div>
+                  <div className={styles.en_name}>微信号:{wxName && wxName || ''}</div>
+                  <div className={styles.en_name}>地区:{address && address || ''}</div>
                 </div>
               </div>
             </div>
             <div>
-              <img className={styles.sex_icon}  src={star} alt="" />
+              <img className={styles.sex_icon} src={star} alt="" />
             </div>
           </div>
         </Card.Body>
--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'dva';
+import router from 'umi/router';
 import HeadCard from '../../components/HeaderCard/index'
 import sexIcon from '../../assets/女.png'
 import headImage from '../../assets/WechatIMG34.jpeg'
@@ -8,7 +9,7 @@ import friends2 from '../../assets/headImage/WechatIMG47.jpeg'
 import friends3 from '../../assets/headImage/WechatIMG48.jpeg'
 import video from '../../assets/视频 (1).png'
 import message from '../../assets/聊天 (1).png'
-import { List,Card,Button ,WhiteSpace} from 'antd-mobile'
+import { List, Card, Button, WhiteSpace, NavBar, Icon } from 'antd-mobile'
 import styles from './index.less'
 @connect(({ detail }) => ({ detail }))
 class Detail extends Component {
@@ -20,29 +21,49 @@ class Detail extends Component {
   componentDidMount() {
   }
   render() {
+    const props = {
+      headImage: headImage,
+      sexIcon: sexIcon,
+      nickName: "Sweetheart❤恋人",
+      name: "ღ",
+      wxName: "micomxx",
+      address: "江苏 南京"
+    }
     return (
       <div style={{ flex: 1, height: '100%', width: "100%", background: "#F5F5F5" }}>
-        <HeadCard headImage={headImage} sexIcon={sexIcon} />
+        {/* 导航栏 */}
+        <NavBar mode="dark" icon={<Icon type="left" />} onLeftClick={() => router.goBack()}
+          rightContent={[
+            <Icon key="1" type="ellipsis" onClick={()=>{ }} />,
+          ]}
+          style={{background:"#F5F5F5",color:"black"}}
+        >
+          {props.nickName}
+        </NavBar>
+        {/* 个人信息卡片 */}
+        <HeadCard {...props} />
+        {/* 列表 */}
         <List>
           <List.Item arrow="horizontal" onClick={() => { }}>备注和标签</List.Item>
           <List.Item arrow="horizontal" onClick={() => { }}>朋友圈权限</List.Item>
         </List>
-        <WhiteSpace size="lg"/>
+        <WhiteSpace size="lg" />
         <Card>
-            <Card.Body style={{ paddingTop: 20, paddingRight: 20, paddingLeft: 20, paddingBottom: 20 }}>
-              <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                <div className={styles.real_name}>朋友圈</div>
-                <img className={styles.head_logo} src={headImage} alt="" />
-                <img className={styles.head_logo} src={friends} alt="" />
-                <img className={styles.head_logo} src={friends2} alt="" />
-                <img className={styles.head_logo} src={friends3} alt="" />
-              </div>
-            </Card.Body>
-          </Card>
+          <Card.Body style={{ paddingTop: 20, paddingRight: 20, paddingLeft: 20, paddingBottom: 20 }}>
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+              <div className={styles.real_name}>朋友圈</div>
+              <img className={styles.head_logo} src={headImage} alt="" />
+              <img className={styles.head_logo} src={friends} alt="" />
+              <img className={styles.head_logo} src={friends2} alt="" />
+              <img className={styles.head_logo} src={friends3} alt="" />
+            </div>
+          </Card.Body>
+        </Card>
         <List>
           <List.Item arrow="horizontal" onClick={() => { }}>更多信息</List.Item>
         </List>
         <WhiteSpace size="lg" />
+        {/* 按钮 */}
         <Button icon={<img src={message} alt="" />} className={styles.button_font} >发送消息</Button>
         <Button icon={<img src={video} alt="" />} className={styles.button_font} >音视频通话</Button>
       </div>
