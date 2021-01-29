506b8818a423eb0e5b8d9b616c40f20fa8499fbe
WX <42308734@qq.com>
2021-01-29 16:30:41 +0800

    fix:封装图片预览旋转

---
+++ src/components/PreviewImage/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { Modal } from 'antd-mobile';
+import currentImage from '../../assets/WechatIMG34.jpeg'
+import styles from './index.less'
+import scroll from '../../assets/旋转 .png'
+class PreviewImage extends Component {
+  // 取消预览，这里需要将下一次旋转的初始值0,rotate(0deg)
+  cancelPreview = () => {
+    this.props.setDate({
+      type:"onClose",
+      visible: false,
+      current: 90,
+      transStyle: 'rotate(' + 0 + 'deg)'
+    })
+  }
+  //  点击选择  设置当前current旋转角度为上一次+90°
+  translate = () => {
+    const { current } = this.props
+    this.props.setDate({
+      type:"translate",
+      current: (current + 90) % 360,
+      transStyle: 'rotate(' + current + 'deg)'
+    });
+  }
+
+  render() {
+    const { visible, previewImage, transStyle } = this.props
+    const currentSrc = previewImage ? previewImage : currentImage
+    console.log('transStyle',transStyle)
+    return (
+        <Modal
+          closable
+          visible={visible}
+          maskClosable={true}
+          footer={[
+            {
+              text: '旋转',
+              onPress:this.translate.bind(this)
+            }
+          ]}
+          onClose={this.cancelPreview.bind(this)}
+          className={styles.modal_card}
+        >
+          <div style={{ transform: transStyle}}>
+            <img
+              alt=""
+              style={{ width: '100%', height: '100%' }}
+              src={currentSrc}
+            />
+            <div style={{top:'50%',right:'50%',position:"absolute"}} onClick={this.translate.bind(this)}>
+              <img alt="" src={scroll} style={{height:20,width:20}} />
+            </div>
+          </div>
+        </Modal>
+    )
+  }
+
+}
+export default PreviewImage
--- src/pages/moments/index.js
+++ src/pages/moments/index.js
@@ -7,16 +7,29 @@ import headImage from '../../assets/WechatIMG34.jpeg'
 import headImage2 from '../../assets/headImage/WechatIMG37.jpeg'
 import headImage3 from '../../assets/headImage/WechatIMG38.jpeg'
 import set from '../../assets/更多,三个点.png'
+import PreviewImage from '../../components/PreviewImage/index'
 @connect(({ moments }) => ({ moments }))
 class Moments extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      visible: false,
+      previewImage: '',
+      current: 90,
+      transStyle: ''
     }
   }
   componentDidMount() {
   }
+
+  preview = (src) =>{
+    this.setState({
+      visible:true,
+      previewImage:src
+    })
+  }
   render() {
+    const { visible, previewImage, current, transStyle } = this.state
     return (
       <div style={{ flex: 1, height: "100%", width: "100%", background: "#fff" }}>
         {/* 朋友圈背景 */}
@@ -39,20 +52,20 @@ class Moments extends Component {
                 <div style={{ wordBreak: "break-all", width: "100%" }}>❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤</div>
                 {/* 图片 */}
                 <div style={{ display: "flex", flexWrap: "wrap", flexDirection: 'row', width: "60%" }}>
-                  <img alt="" src={headImage} className={styles.head_Image} style={{ marginRight: 5, marginBottom: 5 }} />
-                  <img alt="" src={headImage2} className={styles.head_Image} style={{ marginRight: 5, marginBottom: 5 }} />
-                  <img alt="" src={headImage3} className={styles.head_Image} style={{ marginRight: 5, marginBottom: 5 }} />
-                  <img alt="" src={headImage2} className={styles.head_Image} style={{ marginRight: 5, marginBottom: 5 }} />
+                  <img alt="" src={headImage} className={styles.head_Image} style={{ marginRight: 5, marginBottom: 5 }} onClick={this.preview.bind(this,headImage)} />
+                  <img alt="" src={headImage2} className={styles.head_Image} style={{ marginRight: 5, marginBottom: 5 }} onClick={this.preview.bind(this,headImage2)} />
+                  <img alt="" src={headImage3} className={styles.head_Image} style={{ marginRight: 5, marginBottom: 5 }} onClick={this.preview.bind(this,headImage3)} />
+                  <img alt="" src={headImage2} className={styles.head_Image} style={{ marginRight: 5, marginBottom: 5 }} onClick={this.preview.bind(this,headImage2)} />
                 </div>
                 {/* 底部 */}
-                <div style={{ marginTop: 5,display:"flex",justifyContent:"space-between" }}>
+                <div style={{ marginTop: 5, display: "flex", justifyContent: "space-between" }}>
                   <div style={{ color: "#A9A9A9" }}>1小时</div>
                   <img alt="" src={set} style={{ width: 20, height: 20 }} />
                 </div>
                 {/* 留言 */}
                 <div className={styles.remark_container}>
-                    <span className={styles.remark_font}>micomxx:</span>
-                    <span style={{marginLeft:5}}>❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤</span>
+                  <span className={styles.remark_font}>micomxx:</span>
+                  <span style={{ marginLeft: 5 }}>❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤❤</span>
                 </div>
               </div>
             </div>
@@ -77,19 +90,33 @@ class Moments extends Component {
                   <img alt="" src={headImage2} className={styles.head_Image} style={{ marginRight: 5, marginBottom: 5 }} />
                 </div>
                 {/* 底部 */}
-                <div style={{ marginTop: 5,display:"flex",justifyContent:"space-between" }}>
+                <div style={{ marginTop: 5, display: "flex", justifyContent: "space-between" }}>
                   <div style={{ color: "#A9A9A9" }}>1小时</div>
                   <img alt="" src={set} style={{ width: 20, height: 20 }} />
                 </div>
                 {/* 留言 */}
                 <div className={styles.remark_container}>
-                    <span className={styles.remark_font}>micomxx:</span>
-                    <span style={{marginLeft:5}}>wx❤mx</span>
+                  <span className={styles.remark_font}>micomxx:</span>
+                  <span style={{ marginLeft: 5 }}>wx❤mx</span>
                 </div>
               </div>
             </div>
           </Card.Body>
         </Card>
+        <PreviewImage
+          visible={visible}
+          previewImage={previewImage}
+          current={current}
+          transStyle={transStyle}
+          setDate={(value)=>{
+            const {visible,current,transStyle,type} = value
+            this.setState({
+              visible:type==='onClose'?visible:true,
+              current:current,
+              transStyle:transStyle
+            })
+          }}
+        />
       </div>
     )
   }
