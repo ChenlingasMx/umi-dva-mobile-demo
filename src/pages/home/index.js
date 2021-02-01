b8db2fd86094e5343456cbfda4890562b427aba0
WX <42308734@qq.com>
2021-02-01 15:36:39 +0800

    fix:聊天页面

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,49 +6,54 @@ import head from '../../assets/WechatIMG34.jpeg'
 import head2 from '../../assets/headImage/WechatIMG37.jpeg'
 import head3 from '../../assets/headImage/WechatIMG48.jpeg'
 import head4 from '../../assets/headImage/WechatIMG47.jpeg'
+import noise from '../../assets/静音.png'
 @connect(({ home, login }) => ({ home, login }))
 class Home extends Component {
   state = {
     imgList: [
       {
-        img:head,
-        name:"Sweetheart❤恋人"
+        img: head,
+        name: "Sweetheart❤恋人",
       },
       {
-        img:head2,
-        name:"🌞"
+        img: head2,
+        name: "🌞",
+        noise:noise
       },
       {
-        img:head3,
-        name:"🌛"
+        img: head3,
+        name: "🌛",
+        noise:noise
       },
       {
-        img:head4,
-        name:"✨"
+        img: head4,
+        name: "✨",
+        noise:noise
       },
       {
-        img:head2,
-        name:"🌟"
+        img: head2,
+        name: "🌟",
       },
       {
-        img:head,
-        name:"Sweet"
+        img: head,
+        name: "Sweet",
       },
       {
-        img:head2,
-        name:"Heart"
+        img: head2,
+        name: "Heart",
       },
       {
-        img:head3,
-        name:"Love"
+        img: head3,
+        name: "Love"
       },
       {
-        img:head4,
-        name:"❤️"
+        img: head4,
+        name: "❤️"
       },
       {
-        img:head2,
-        name:"🌟"
+        img: head2,
+        name: "🌟",
+        noise:noise
       },
     ]
   }
@@ -56,13 +61,19 @@ class Home extends Component {
     return (
       <div style={{ flex: 1, height: "100%", width: "100%", background: "#F5F5F5" }}>
         {/* 搜索栏 */}
-        <SearchBar placeholder="搜索" maxLength={8}  />
+        <SearchBar placeholder="搜索" maxLength={8} />
         {/* 卡片 */}
         {this.state.imgList.map((itm, index) => (
-          <List  key={index} className={styles.list_item}>
+          <List key={index} className={styles.list_item}>
             <List.Item
               thumb={<img src={itm.img} alt="" className={styles.headImage} />}
-              extra={'15:14'}
+              extra={
+                <div>
+                  <div>15:13</div>
+                  {itm.noise&&<img src={itm.noise} alt="" style={{height:15,width:15}} />}
+                </div>
+              }
+              onClick={() => { }}
             >
               <div>
                 <span style={{ fontSize: 20 }}>{itm.name}</span>
